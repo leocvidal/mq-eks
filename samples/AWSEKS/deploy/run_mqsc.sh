c4b8165910a969c86ba7a959b700750c53355a42
leocvidal <leo.vidal@au1.ibm.com>
2025-05-22 20:04:40 +1000

    Update run_mqsc.sh

--- samples/AWSEKS/deploy/run_mqsc.sh
+++ samples/AWSEKS/deploy/run_mqsc.sh
@@ -8,7 +8,7 @@ MQ_URL="https://${LB}:9443/ibmmq/rest/v2/admin/action/qmgr/secureapphelm/mqsc"
 ERROR_LOG="mqsc_errors.log"
 
 echo $MQ_URL
-MYURL="https://${LB}:9443/ibmmq/rest/v2/admin/qmgr")
+MYURL="https://${LB}:9443/ibmmq/rest/v2/admin/qmgr"
 echo $MYURL
 
 for i in {1..30}; do
